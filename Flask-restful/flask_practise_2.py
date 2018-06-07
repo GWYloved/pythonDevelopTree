131d897a1cd6823ce1be80e4a69d609f5af2f942
GWYloved <913925047@qq.com>
2018-06-07 19:37:37 +0800

    hehe

--- Flask-restful/flask_practise_2.py
+++ Flask-restful/flask_practise_2.py
@@ -1,5 +1,6 @@
-from flask import Flask
-
+from flask import Flask,abort
+import datetime
+import time
 app = Flask(__name__)
 
 @app.route('/')
@@ -7,8 +8,20 @@ def index():
 	return  'Hello, world'
 
 @app.route('/gg')
-def index():
+def gg():
 	return  'gg'
+
+@app.route('/time')
+def ti():
+	return datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+@app.route('/404')
+def err_404():
+	abort(404)
+
+@app.route('/300')
+def err_300():
+	abort(300)
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0',debug=True)
